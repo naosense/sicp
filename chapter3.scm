fe9f111cde3a70f4357875bc3d11bb57da8995c4
pingao <pingao777@gmail.com>
2017-12-28 18:22:18 +0800

    添加chapter3

---
+++ chapter3.scm
@@ -0,0 +1,26 @@
+(define (withdraw amount)
+  (if (>= balance amount)
+    (begin (set! balance (- balance amount))
+           balance)
+    "Insufficient funds"))
+
+(define balance 100)
+
+(define new-withdraw
+  (let ((balance 100))
+    (lambda (amount)
+      (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))))
+
+(define (make-withdraw balance)
+  (lambda (amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      "Insufficient funds")))
+
+(define W1 (make-withdraw 100))
+
+(define W2 (make-withdraw 100))
