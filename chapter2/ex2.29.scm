7ae60a7a5da540ca6723f3db6628fa05985b9490
wocanmei <pingao777@gmail.com>
2019-01-27 19:41:39 +0800

    ex2.29

--- chapter2/ex2.29.scm
+++ chapter2/ex2.29.scm
@@ -20,6 +20,7 @@
 (define (branch-structure b)
   (car (cdr b)))
 
+;; 原版有些繁琐
 ;(define (total-weight m)
 ;  (let ((left-weight (branch-structure (left-branch m)))
 ;        (right-weight (branch-structure (right-branch m))))
@@ -40,33 +41,31 @@
 ;           (+ (total-weight left-weight)
 ;              (total-weight right-weight))))))
 
+;; 网上的比较精简/巧妙
 (define (total-weight mobile) 
-   (cond ((null? mobile) 0) 
-         ((not (pair? mobile)) mobile) 
-         (else (+ (total-weight (branch-structure (left-branch mobile))) 
-                  (total-weight (branch-structure (right-branch mobile)))))))
+  (cond ((null? mobile) 0) 
+        ((not (pair? mobile)) mobile) 
+        (else (+ (total-weight (branch-structure (left-branch mobile))) 
+                 (total-weight (branch-structure (right-branch mobile)))))))
 
+;; 左右分支平衡且各自的子分支平衡
 (define (balanced? mobile)
   (define (torque branch) 
-   (* (branch-length branch) (total-weight (branch-structure branch)))) 
-   (if (not (pair? mobile)) 
-       true 
-       (and (= (torque (left-branch mobile)) (torque (right-branch mobile))) 
-            (balanced? (branch-structure (left-branch mobile))) 
-            (balanced? (branch-structure (right-branch mobile)))))) 
+    (* (branch-length branch) (total-weight (branch-structure branch)))) 
+  (if (not (pair? mobile)) 
+      true 
+      (and  (balanced? (branch-structure (left-branch mobile))) 
+            (balanced? (branch-structure (right-branch mobile)))
+            (= (torque (left-branch mobile)) (torque (right-branch mobile)))))) 
 
 
-  
-          
-          
-  
 
 (total-weight (make-mobile (make-branch 7 (make-mobile (make-branch 2 5)
                                                        (make-branch 7 5)))
                            (make-branch 3 (make-mobile (make-branch 2 2)
                                                        (make-branch 7 1)))))
 
-(balance? (make-mobile (make-branch 7 (make-mobile (make-branch 2 5)
-                                                       (make-branch 7 5)))
-                           (make-branch 3 (make-mobile (make-branch 2 2)
-                                                       (make-branch 7 1)))))
+(balanced? (make-mobile (make-branch 3 (make-mobile (make-branch 2 7)
+                                                    (make-branch 7 2)))
+                        (make-branch 3 (make-mobile (make-branch 2 7)
+                                                    (make-branch 7 2)))))
