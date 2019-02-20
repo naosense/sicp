bfdf713454163aaacc3c32dc13eadd5291e2ab8b
wocanmei <pingao777@gmail.com>
2019-02-20 18:01:29 +0800

    ex2.59-ex2.60

--- chapter2/ex2.60.scm
+++ chapter2/ex2.60.scm
@@ -1,19 +1,22 @@
 #lang sicp
 
+; O(n)
 (define (element-of-set? x set)
   (cond ((null? set) false)
         ((equal? x (car set)) true)
         (else (element-of-set? x (cdr set)))))
-
+; O(1)
 (define (adjoin-set x set)
   (cons x set))
 
+; O(n1*n2)
 (define (intersection-set set1 set2)
   (cond ((or (null? set1) (null? set2)) '())
         ((element-of-set? (car set1) set2)
          (cons (car set1) (intersection-set (cdr set1) set2)))
         (else (intersection-set (cdr set1) set2))))
 
+; O(n1+n2)
 (define (union-set set1 set2)
   (append set1 set2))
 
