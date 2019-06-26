c144c7c76d0062f9c3bd68aaf259e2e68e73d503
wocanmei <pingao777@gmail.com>
2019-06-26 18:18:55 +0800

    修改说明文档

--- info.rkt
+++ info.rkt
@@ -59,7 +59,20 @@
 (define (make-chapter chapter-idx)
   (string-append "chapter" (number->string chapter-idx)))
 
-(define width 7)
+(define width 15)
+
+(define (progress total done)
+  (let ((len 60)
+        (percent (/ done total)))
+    (string-append "|"
+                   (string-join (enumerate-n "=" (floor (* len percent))) "")
+                   "|"
+                   (number->string (floor (* percent 100)))
+                   "% ("
+                   (number->string done)
+                   "/"
+                   (number->string total)
+                   ")")))
 
 (define readme-content
   (string-append
@@ -67,27 +80,54 @@
 
 编程环境为DrRacket，版本7.1，[配置方法](https://docs.racket-lang.org/sicp-manual/)，没有选择mit-scheme，原因有二：
 - 编辑和调试不好用
-- 没有sicp图形语言"
-   "\n\n习题进度\n\n"
+- 没有sicp图形语言
+
+### 遇到问题
+
+#### 图形语言
+
+sicp-pict包实现的图形语言有些习题不能做，可以使用`chapter2/pict.scm`代替。
+
+#### 惰性求值
+
+和mit-scheme不同，书本上的`delay`函数在racket中并不能延迟求值，可以用下面的代码实现：
+
+```scheme
+(define-syntax delay
+  (syntax-rules ()
+    ((delay expr)
+     (memo-proc
+      (lambda ()
+        expr)))))
+```
+
+在流与延时求值那一节，课本上的solve函数不能运行，可以作如下修改：
+
+```scheme
+(define (solve f y0 dt)
+  (define y (integral (delay (force dy)) y0 dt))
+  (define dy (delay (stream-map f y)))
+  y)
+```
+"
+   "\n\n### 习题进度\n\n"
    (string-join
    (map
     (lambda (chapter-idx)
       (let ((done (filter execise-file? (list-path-string (make-chapter chapter-idx))))
             (total-count (list-ref count-list (- chapter-idx 1))))
         (string-append
-         (string-append "第"
+         (string-append "#### 第"
                         (number->string chapter-idx)
-                        "章：总量"
-                        (number->string total-count)
-                        "，完成"
-                        (number->string (length done))
+                        "章："
+                        (progress total-count (length done))
                         "\n\n")
          (string-join (append (list (make-row (map number->string (enumerate-interval 1 width)))
                                     (make-row (enumerate-n ":---:" width)))
                               (map make-row
                                    (split (map (lambda (ex-idx)
                                                  (let ((ex (make-ex chapter-idx ex-idx)))
-                                                   (if (member ex done) ex "--")))
+                                                   (if (member ex done) (substring ex 2 6) "--")))
                                                (enumerate-interval 1 total-count))
                                           width)))
                       "\n"))))
